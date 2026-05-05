f2f18c089a867ab4ce55b2fb5777d4a10852d19b
leo-gad123 <hakizimanaleogad@gmail.com>
2026-05-05 15:30:59 +0200

    updated files

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.stocknova.app',
   appName: 'stockNova',
-  webDir: 'dist'
+  webDir: 'build'
 };
 
 export default config;
